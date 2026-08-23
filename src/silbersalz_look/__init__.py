4deeae96d4ffc5abcd8ec917d0ded7890b8896e6
atrouwee <adriaan.trouwee@gmail.com>
2026-08-23 07:02:29 +0200

    rename remaining references to open-silbersalz

--- src/silbersalz_look/__init__.py
+++ src/silbersalz_look/__init__.py
@@ -1,3 +1,3 @@
-"""silbersalz-look: community reverse-engineering of the SILBERSALZ35 grade."""
+"""open-silbersalz: community reverse-engineering of the SILBERSALZ35 grade."""
 
 __version__ = "0.1.0"
